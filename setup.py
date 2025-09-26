83e5d4455b33c89e579f704cb2d10b2a11f81fc4
Rajrantan More <Rajratan.More@EMERGYS.LOCAL>
2025-09-26 20:03:38 +0530

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='data_science_pro',
-    version='0.1.8',
+    version='0.1.9',
     description='LLM-powered modular data science pipeline',
     long_description=long_description,
     long_description_content_type='text/markdown',
