5f09ef5f617b8ed5e6b93e5dd11d01c661458a99
Rajrantan More <Rajratan.More@EMERGYS.LOCAL>
2025-09-26 14:32:42 +0530

    changes setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='data_science_pro',
     version='0.1.0',
     description='LLM-powered modular data science pipeline',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Your Name',
     packages=find_packages(),
     install_requires=[
